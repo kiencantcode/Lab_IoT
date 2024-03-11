0003bb63eb9bb1b759287ba3068d7eea105dcdc7
EmKien <kienbk09052003@gmail.com>
2024-03-11 20:29:14 +0700

    update commit of lab3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from uart import *
 
 AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "EmKien"
-AIO_KEY = "aio_ZEvE20yeiEfRSw2zPA7vNOcO9P29"
+AIO_KEY = "aio_xUnN56dJmTwGivGbsxOuIfPVEcpm"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -23,6 +23,16 @@ def disconnected(client):
 
 def message(client , feed_id , payload):
     print("Nhan du lieu: " + payload + ", feed id: " + feed_id)
+    if feed_id == "nutnhan1":
+        if payload == "1":
+            writeData("1")
+        else:
+            writeData("2")
+    if feed_id == "nutnhan2":
+        if payload == "1":
+            writeData("3")
+        else:
+            writeData("4")
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -31,12 +41,14 @@ client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
-# counter = 10
-# counter_ai = 2
+counter = 5
+counter_ai = 5
+ai_result_str = ""
+previous_result = ""
 while True:
     # counter = counter - 1
     # if counter <= 0:
-    #     counter = 10
+    #     counter = 5
     #
     #     print("Random data is publishing...")
     #     temp = random.randint(10,20)
@@ -45,13 +57,16 @@ while True:
     #     client.publish("cambien2", humi)
     #     light = random.randint(100, 500)
     #     client.publish("cambien3", light)
-
-    # counter_ai = counter_ai - 1
-    # if counter_ai <= 0:
-    #     counter_ai = 2
-    #     ai_result = image_detector()
-    #     ai_result_str = str(ai_result)
-    #     print("Final result: ", ai_result_str)
+    #
+    counter_ai = counter_ai - 1
+    if counter_ai <= 0:
+        counter_ai = 5
+        previous_result = ai_result_str
+        ai_result = image_detector()
+        ai_result_str = str(ai_result)
+        print("AI Output: ", ai_result_str)
+        if previous_result != ai_result_str:
+            client.publish("ai", ai_result_str)
     readSerial(client)
     time.sleep(1)
 
