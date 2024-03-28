75b93f4081bb7c0975ba049362687be618c0a028
EmKien <kienbk09052003@gmail.com>
2024-03-28 21:39:40 +0700

    first commit for lab1

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import sys
 from Adafruit_IO import MQTTClient
 import time
 import random
-from simple_ai import *
-from uart import *
+# from simple_ai import *
 
 AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "EmKien"
-AIO_KEY = "aio_xNni93cL6GGNpzJLJ396AYc0xpq4"
+AIO_KEY = "aio_BXFt26rNWDXPUJvHNEMCqs1azqWI"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -23,16 +22,6 @@ def disconnected(client):
 
 def message(client , feed_id , payload):
     print("Nhan du lieu: " + payload + ", feed id: " + feed_id)
-    if feed_id == "nutnhan1":
-        if payload == "1":
-            writeData("1")
-        else:
-            writeData("2")
-    if feed_id == "nutnhan2":
-        if payload == "1":
-            writeData("3")
-        else:
-            writeData("4")
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -41,28 +30,20 @@ client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
-# counter = 10
-# counter_ai = 2
+counter = 10
+# counter_ai = 10
 while True:
-    # counter = counter - 1
-    # if counter <= 0:
-    #     counter = 10
-    #
-    #     print("Random data is publishing...")
-    #     temp = random.randint(10,20)
-    #     client.publish("cambien1", temp)
-    #     humi = random.randint(50, 70)
-    #     client.publish("cambien2", humi)
-    #     light = random.randint(100, 500)
-    #     client.publish("cambien3", light)
+    counter = counter - 1
+    if counter <= 0:
+        counter = 10
 
-    # counter_ai = counter_ai - 1
-    # if counter_ai <= 0:
-    #     counter_ai = 2
-    #     ai_result = image_detector()
-    #     ai_result_str = str(ai_result)
-    #     print("Final result: ", ai_result_str)
-    readSerial(client)
+        print("Random data is publishing...")
+        temp = random.randint(10,20)
+        client.publish("cambien1", temp)
+        humi = random.randint(50, 70)
+        client.publish("cambien2", humi)
+        light = random.randint(100, 500)
+        client.publish("cambien3", light)
+
     time.sleep(1)
-
     pass
